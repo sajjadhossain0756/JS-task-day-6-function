320dd714c68669ceac3caad79f33b86271d31d96
sajjadhossain <sajjadhossain0756@gmail.com>
2024-09-07 12:24:07 +0600

    task-1 complete

---
+++ index.js
@@ -0,0 +1,14 @@
+// JS function task start here;
+// task-1 start here;
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiplyNumber (num1,num2,num3,num4){
+      result = num1 * num2 * num3 * num4;
+      return result;
+}
+
+const Multiply = multiplyNumber(2,5,3,2);
+const Multiply1 = multiplyNumber(2,5,9,2);
+
+console.log(Multiply);
+console.log(Multiply1);
